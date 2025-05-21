0a04425fc66d2b4e84afc4c25eb4d510f9dc73be
RustyRishii <rishikeshbidkar04@gmail.com>
2025-05-21 16:15:40 +0530

    Initial Commit

---
+++ StashIt/StashItApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashItApp.swift
+//  StashIt
+//
+//  Created by Rishikesh Bidkar on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct StashItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StashItTests/StashItTests.swift
@@ -0,0 +1,17 @@
+//
+//  StashItTests.swift
+//  StashItTests
+//
+//  Created by Rishikesh Bidkar on 21/05/25.
+//
+
+import Testing
+@testable import StashIt
+
+struct StashItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
